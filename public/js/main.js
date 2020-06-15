cd9bc77e099321b422fd75d586ea7931620c502e
zandonacamila <zandona.camila@gmail.com>
2020-06-15 14:14:36 -0300

    Progresso de 15/06

--- public/js/main.js
+++ public/js/main.js
@@ -4,4 +4,26 @@ var tamanhoFrase = $("#tamanho-frase");
 
 tamanhoFrase.text(numPalavras);
 
-console.log(tamanhoFrase);
+var campo = $(".campo-digitacao");
+campo.on("input", function() {
+    var conteudo = campo.val();
+    
+    var qtdPalavras = conteudo.split(/\S+/).length - 1;
+    $("#contador-palavras").text(qtdPalavras);
+
+    var qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+});
+
+var tempoRestante = $("#tempo-digitacao").text();
+campo.one("focus", function() {
+    var cronometroId = setInterval(function(){
+        tempoRestante--;
+        // console.log(tempoRestante)
+        $("#tempo-digitacao").text(tempoRestante);
+        if(tempoRestante < 1) {
+            campo.attr("disabled", true);
+            clearInterval(cronometroId)
+        }
+    }, 1000);
+});
