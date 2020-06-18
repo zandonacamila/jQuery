8a7ac83ac8a9be64ae08ade51117004931e920fb
zandonacamila <zandona.camila@gmail.com>
2020-06-18 15:05:00 -0300

    Melhora na experiência do usuário com a adição de um 'spinner'

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,13 +1,24 @@
 $("#botao-frase").click(fraseAleatoria);
 
 function fraseAleatoria() {
-    $.get("http://localhost:3000/frases", trocaFraseAleatoria);
+    $("#spinner").show();
+    
+    $.get("http://localhost:3000/frases", trocaFraseAleatoria)
+    .fail(function() {
+        $("#erro").toggle();
+        setTimeout(function() {
+            $("#erro").toggle();
+        }, 2000);
+    })
+    .always(function() {
+        $("#spinner").toggle();
+    });
 }
 
 function trocaFraseAleatoria(data) {
-    var frase = $(".frase");
+    var fraseNova = $(".frase");
     var numeroAleatorio = Math.floor(Math.random() * data.length);
-    frase.text(data[numeroAleatorio].texto);
+    fraseNova.text(data[numeroAleatorio].texto);
     atualizaTamanhoFrase();
     atualizaTempoInicial(data[numeroAleatorio].tempo);
 }
